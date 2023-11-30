6bc841e23e62025d2d36af5740c32a26986dd838
tornike <tornike.tanda@gmail.com>
2023-11-30 05:19:35 -0800

    first mit

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ const PORT = process.env.PORT || 5000;
 //     useUnifiedTopology: true,
 //   }
 // );
-app.use(cors({ credentials: true, origin: "https://pizza-adw3.onrender.com" }));
+// app.use(cors({ credentials: true, origin: "https://pizza-adw3.onrender.com" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://pizza-adw3.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json()); // Add this line to parse JSON in the request body
 app.use(cookieParser());
 app.use("/images", express.static(path.join(__dirname, "images")));
